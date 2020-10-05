a66741bd49146f18b0a07b4971485101b98b3478
silinh2002 <silinh.200298@gmail.com>
2020-10-05 15:11:12 +0700

    Updates

---
+++ precache-manifest.b95ff08e27108fea7d41b53ea0dd9a96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a895cbc53e342d6960229b74a5107118",
+    "url": "/Intro-to-React/index.html"
+  },
+  {
+    "revision": "61f809f6dec294d33467",
+    "url": "/Intro-to-React/static/css/main.00cd9408.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/Intro-to-React/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Intro-to-React/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f809f6dec294d33467",
+    "url": "/Intro-to-React/static/js/main.4326492f.chunk.js"
+  },
+  {
+    "revision": "da6b91eca388422c7d0e",
+    "url": "/Intro-to-React/static/js/runtime-main.e19769d9.js"
+  }
+]);
