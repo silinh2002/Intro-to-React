f5df4f362434616b3c24dcafe21c3c6f15bc8eb5
silinh2002 <silinh.200298@gmail.com>
2020-10-21 21:48:36 +0700

    BTCN03

--- src/index.js
+++ src/index.js
@@ -1,135 +1,113 @@
 import React from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className={"square " + this.props.className}
-        onClick={this.props.onClick}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+import * as serviceWorker from "./serviceWorker";
+function Square(props) {
+  return (
+    <button className={"square " + props.className} onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
-
-class Board extends React.Component {
-  renderSquare(i) {
-    let className = this.props.lines.includes(i) ? "square-win" : "";
-    if (this.props.squares[i]) {
-      className += this.props.squares[i] === "X" ? " square-x" : " square-o";
+function Board(props) {
+  const [row, setRow] = useState([0, 1, 2]);
+  const renderSquare = (i) => {
+    let className = props.lines.includes(i) ? "square-win" : "";
+    if (props.squares[i]) {
+      className += props.squares[i] === "X" ? " square-x" : " square-o";
     }
-
     return (
       <Square
         className={className}
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
       />
     );
-  }
-
-  render() {
-    let row = [0, 1, 2];
-    const element = row.map((value) => (
-      <div key={value} className="board-row">
-        {this.renderSquare((value % 3) * 3)}
-        {this.renderSquare((value % 3) * 3 + 1)}
-        {this.renderSquare((value % 3) * 3 + 2)}
-      </div>
-    ));
-    return <div>{element}</div>;
-  }
+  };
+  const element = row.map((value) => (
+    <div key={value} className="board-row">
+      {renderSquare((value % 3) * 3)}
+      {renderSquare((value % 3) * 3 + 1)}
+      {renderSquare((value % 3) * 3 + 2)}
+    </div>
+  ));
+  return <div>{element}</div>;
 }
-
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.position = 0;
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null),
-        },
-      ],
-      stepNumber: 0,
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    this.position = i;
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+function Game(props) {
+  const [state, setState] = useState({
+    history: [{ squares: Array(9).fill(null) }],
+    stepNumber: 0,
+    xIsNext: true,
+    isStart: false,
+    player: null,
+    operatorClick: false,
+    // position: 0,
+    backStep: null,
+    lastX: null,
+    lastO: null,
+  });
+  let handleClick = (i) => {
+    // let position ;
+    const history = state.history.slice(0, state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
+    squares[i] = state.xIsNext ? "X" : "O";
+    setState({
       history: history.concat([
         {
           squares: squares,
         },
       ]),
       stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
+      xIsNext: !state.xIsNext,
     });
-  }
-
-  jumpTo(step) {
-    this.setState({
+  };
+  let jumpTo = (step) => {
+    setState({
       stepNumber: step,
       xIsNext: step % 2 === 0,
     });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = "Winner: " + winner.playerWin;
-    } else {
-      if (history.length > 9) status = "No winner: X and O tied ";
-      else status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
+  };
+  const history = state.history;
+  const current = history[state.stepNumber];
+  const winner = calculateWinner(current.squares);
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            lines={winner ? winner.lines : []}
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
     );
+  });
+  let status;
+  if (winner) {
+    status = "Winner: " + winner.playerWin;
+  } else {
+    if (history.length > 9) status = "No winner: X and O tied ";
+    else status = "Next player: " + (state.xIsNext ? "X" : "O");
   }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          lines={winner ? winner.lines : []}
+          squares={current.squares}
+          onClick={(i) => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 }
-
 // ========================================
-
 ReactDOM.render(<Game />, document.getElementById("root"));
-
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -152,3 +130,10 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+//ReactDOM.render(<Game />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
